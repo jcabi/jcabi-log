baba8f5e3cc3f4c50a680166d71816058e74768e
uchitsa <uchitsa@ya.ru>
2024-12-03 11:56:08 +0300

    fix quality violations

--- src/test/java/com/jcabi/log/SupplierLoggerTest.java
+++ src/test/java/com/jcabi/log/SupplierLoggerTest.java
@@ -95,6 +95,7 @@ final class SupplierLoggerTest {
             (Supplier<String>) () -> "test3"
         );
         MatcherAssert.assertThat(
+            "debug should be disabled",
             ((UnitTestAppender) logger.getAppender(appender)).output(),
             Matchers.emptyString()
         );
